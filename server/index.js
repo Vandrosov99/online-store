cdb46d42b5c00fda52e458aeb427b5dc57efa16c
Volodymyr Androsov <v.androsov@pnn.in.ua>
2021-10-13 00:52:14 +0300

    connect db to app

--- server/index.js
+++ server/index.js
@@ -2,11 +2,21 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+const sequelize = require("./db");
 
 const PORT = process.env.PORT || 3000;
 
-console.log(PORT);
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
 
-app.listen(PORT, () => {
-  console.log(`Server starts on this post: ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server starts on this post: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
